ea81c0b27ab927b774f570bb22109a75b1f88175
Elias Menon <eliasmenon@gmail.com>
2021-03-07 11:43:48 -0300

    Move chat to the bottom of the window when it's too narrow

--- src/main.js
+++ src/main.js
@@ -3,19 +3,40 @@ const pageManagerContainer = document.getElementsByClassName("ytd-page-manager")
 
 const theaterContainer = document.getElementById("player-theater-container");
 
-const chatFrame = document.getElementById("chatframe");
-
-const primaryColumn = document.getElementById("primary");
+const columns = document.getElementById("columns");
+const meta = document.getElementById("meta");
+const info = document.getElementById("info");
 const related = document.getElementById("related");
 const headerNav = document.getElementById("masthead-container");
 
 const miniPlayer = document.getElementsByClassName("ytd-miniplayer")[0];
 
+var chatWidth = "400px";
+
+var isTheater = false;
+var isOneColumn = false;
+
+var chat = document.getElementById("chat");
+var chatFrame = document.getElementById("chatframe");
+var hideButton = document.getElementById("show-hide-button");
+
+const reloadChatElems = () => {
+    chat = document.getElementById("chat");
+    chatFrame = document.getElementById("chatframe");
+    hideButton = document.getElementById("show-hide-button");
+}
+
+
 const toggleVideoPlayerStyle = () => {
-    if (!document.documentElement.style.overflow) {
+    if (isTheater) {
         document.documentElement.style.overflow = "hidden";
         pageManager.style.marginTop = "0px";
-        theaterContainer.style.cssText = theaterContainer.style.cssText + "width: calc(100% - 400px); height: 100vh; max-height: none;"
+        theaterContainer.style.width = `calc(100% - ${chatWidth})`;
+        theaterContainer.style.height = "100vh";
+        theaterContainer.style.maxHeight = "none";
+        if (isOneColumn) {
+            toggleIsOneColumn();
+        }
     } else {
         document.documentElement.style.overflow = "";
         pageManager.style.marginTop = "";
@@ -26,8 +47,13 @@ const toggleVideoPlayerStyle = () => {
 }
 
 const toggleChatFrameStyle = () => {
-    if (chatFrame.style.height !== "100vh") {
-        chatFrame.style.cssText = chatFrame.style.cssText + "width: 400px; height: 100vh; position: absolute; right: 0px; top: 0px;"
+    reloadChatElems();
+    if (isTheater) {
+        chatFrame.style.width = chatWidth;
+        chatFrame.style.height = "100vh";
+        chatFrame.style.position = "absolute";
+        chatFrame.style.right = "0px";
+        chatFrame.style.top = "0px";
     } else {
         chatFrame.style.width = "";
         chatFrame.style.height = "";
@@ -38,7 +64,7 @@ const toggleChatFrameStyle = () => {
 }
 
 const toggleHideElements = () => {
-    if (!related.style.display) {
+    if (isTheater) {
         related.style.display = "none";
         headerNav.style.display = "none";
     } else {
@@ -49,14 +75,47 @@ const toggleHideElements = () => {
 
 const toggleMode = () => {
     toggleHideElements();
-    toggleVideoPlayerStyle();
     toggleChatFrameStyle();
+    toggleVideoPlayerStyle();
+}
+
+const toggleIsOneColumn = () => {
+    if (isTheater && isOneColumn) {
+        reloadChatElems();
+        theaterContainer.style.width = "100%";
+        theaterContainer.style.height = "";
+        chat.style.marginTop = "0px";
+        chatFrame.style.width = "100%";
+        chatFrame.style.height = "calc(100vh - ((var(--ytd-watch-flexy-height-ratio)/var(--ytd-watch-flexy-width-ratio))*100vw))";
+        chatFrame.style.position = "absolute";
+        chatFrame.style.left = "0px";
+        chatFrame.style.bottom = "0px";
+        chatFrame.style.right = "";
+        chatFrame.style.top = "";
+        columns.style.marginTop = "0px";
+        meta.style.display = "none";
+        info.style.display = "none";
+        hideButton.style.display = "none";
+    } else {
+        toggleVideoPlayerStyle();
+        toggleChatFrameStyle();
+        chat.style.marginTop = "";
+        chatFrame.style.left = "";
+        columns.style.marginTop = "";
+        meta.style.display = "";
+        info.style.display = "";
+        hideButton.style.display = "";
+    }
 }
 
 const handleTheaterMode = (mutationsList) => {
     for (const mutation of mutationsList) {
-        if (mutation.attributeName === 'theater') {
+        if (mutation.attributeName === "theater") {
+            isTheater = !isTheater;
             toggleMode();
+        } else if (mutation.attributeName === "is-two-columns_") {
+            isOneColumn = !isOneColumn;
+            toggleIsOneColumn();
         }
     }
 }
@@ -64,6 +123,11 @@ const handleTheaterMode = (mutationsList) => {
 const theaterToggleObserver = new MutationObserver(handleTheaterMode);
 theaterToggleObserver.observe(pageManagerContainer, { attributes: true });
 
+if (pageManagerContainer.getAttribute("is-two-columns_") == null) {
+    isOneColumn = true
+}
+
 if (theaterContainer.hasChildNodes()) {
+    isTheater = true;
     toggleMode();
 }
